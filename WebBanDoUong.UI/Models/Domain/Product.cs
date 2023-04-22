982e47fde8f518928d1c964f9b78f886617fef45
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2023-04-22 09:28:35 +0700

    remove categoryId in proudct model

--- WebBanDoUong.UI/Models/Domain/Product.cs
+++ WebBanDoUong.UI/Models/Domain/Product.cs
@@ -11,7 +11,7 @@ namespace WebBanDoUong.UI.Models.Domain
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Image { get; set; }
-        public int CategoryId { get; set; }
+        //public int CategoryId { get; set; }
 
         // Navigation property
         public virtual Category Category { get; set; }
